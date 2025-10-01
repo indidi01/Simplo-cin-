4c05056a5d6164a299f02a04f9168ced64121bac
indidi <indian.v01@gmail.com>
2025-10-01 16:56:13 +0200

    carousel moche mais fonctionnel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,21 +1,53 @@
 import type { MovieResult } from "../types/type";
 import MovieCard from "./MovieCard";
 import style from "./Carousel.module.css";
+import { useState } from "react";
 
 interface CarouselProps {
   title: string;
   movies: MovieResult[];
 }
+const MOVIES_PER_PAGE = 5;
 
 const Carousel = ({ title, movies }: CarouselProps) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const totalPages = Math.ceil(movies.length / MOVIES_PER_PAGE);
+
+  const goToNextPage = () => {
+    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+  };
+
+  const goToPrevPage = () => {
+    setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
+  };
+
+  const startIndex = currentPage * MOVIES_PER_PAGE;
+  const endIndex = startIndex + MOVIES_PER_PAGE;
+  const visibleMovies = movies.slice(startIndex, endIndex);
+
   return (
-    <section className={style.carouselSection}>
+    <section className={style.carouselWrapper}>
       <h2 className={style.carouselTitle}>{title}</h2>
+
+      <button
+        className={`${style.scrollButton} ${style.left}`}
+        onClick={goToPrevPage}
+      >
+        &lt;
+      </button>
+
       <div className={style.carouselContent}>
-        {movies.map(movie => (
+        {visibleMovies.map((movie: MovieResult) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
+
+      <button
+        className={`${style.scrollButton} ${style.right}`}
+        onClick={goToNextPage}
+      >
+        &lt;
+      </button>
     </section>
   );
 };
